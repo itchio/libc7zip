1814ee76a0d18bdaf0f0f4015b1363d97948bed4
leaf corcoran <leafot@gmail.com>
2026-01-26 16:20:03 -0800

    add selection for deploy platforms

--- release/ci-deploy-itchio.sh
+++ release/ci-deploy-itchio.sh
@@ -26,6 +26,16 @@ ${TOOLS_DIR}/butler -V
 
 pushd broth
 for i in *; do
+    # Check if this platform should be deployed
+    case "$i" in
+        broth-linux-amd64)  [ "$DEPLOY_LINUX_AMD64" != "true" ] && continue ;;
+        broth-linux-arm64)  [ "$DEPLOY_LINUX_ARM64" != "true" ] && continue ;;
+        broth-windows-386)  [ "$DEPLOY_WINDOWS_386" != "true" ] && continue ;;
+        broth-windows-amd64) [ "$DEPLOY_WINDOWS_AMD64" != "true" ] && continue ;;
+        broth-darwin-amd64) [ "$DEPLOY_DARWIN_AMD64" != "true" ] && continue ;;
+        broth-darwin-arm64) [ "$DEPLOY_DARWIN_ARM64" != "true" ] && continue ;;
+    esac
+
     CHANNEL_NAME="${i}${CHANNEL_SUFFIX}"
     ${TOOLS_DIR}/butler push --userversion "${USER_VERSION}" ./$i "itchio/libc7zip:${CHANNEL_NAME}"
 done
